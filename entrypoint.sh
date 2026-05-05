ed76b968a49a6cef5ae903b9a055593f40f95348
zes <cyril.gerla@protonmail.com>
2026-05-05 10:11:54 +0000

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,10 @@ MESSAGE="$1"
 mkdir -p /output
 
 # Générer un fichier contenant le message
-echo "Votre message : $MESSAGE" > /output/message.txt
+#echo "Votre message : $MESSAGE" > /output/message.txt
 
 # Afficher le message dans les logs
-echo "Message généré : $MESSAGE"
+#echo "Message généré : $MESSAGE"
 
 # Définir la sortie de l'action
 echo "output-message=MESSAGE : $MESSAGE" >> "$GITHUB_OUTPUT"
